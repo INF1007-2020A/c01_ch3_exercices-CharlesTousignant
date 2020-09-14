4368cc8f044ab73cc7faa1cbb19d3ee457d8649c
CharlesTousignant <charles.tousignant@polymtl.ca>
2020-09-14 18:13:44 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -15,7 +15,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 def to_degrees(angle_rads: float) -> tuple:
     temp_degrees = math.degrees(angle_rads)
-    return temp_degrees // 1, (temp_degrees % 1
+    return temp_degrees // 1, (temp_degrees % 1) // 0
 
 
 def to_celsius(temperature: float) -> float:
