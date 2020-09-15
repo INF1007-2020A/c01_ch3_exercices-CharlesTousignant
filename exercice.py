eb18c1e7646e4a487f646031a1f6330e4c0cceec
CharlesTousignant <charles.tousignant@polymtl.ca>
2020-09-15 00:50:09 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -26,7 +26,7 @@ def to_celsius(temperature: float) -> float:
 
 def to_farenheit(temperature: float) -> float:
     farenheit_temperature = temperature * 1.8 + 32
-    return 0.0
+    return farenheit_temperature
 
 
 def main() -> None:
